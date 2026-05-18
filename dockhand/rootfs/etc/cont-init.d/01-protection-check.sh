f4b4f43c889e0582ca9845b570a7387646bd9a7b
Alex Schwantes <2829978+alexschwantes@users.noreply.github.com>
2026-05-18 13:50:21 +1000

    fix: linting issue in bash files

--- dockhand/rootfs/etc/cont-init.d/01-protection-check.sh
+++ dockhand/rootfs/etc/cont-init.d/01-protection-check.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/with-contenv bashio
+# shellcheck shell=bash
 # ==============================================================================
 # Dockhand requires Protection Mode to be disabled in the HA UI.
 # This provides full access to /var/run/docker.sock.
